39bdfd6e41becc7334509c7e909b1ec55b78b659
baileyrechkemmer <rechkemmer3@gmail.com>
2023-03-28 16:15:11 -0500

    fixed my stuff sorry elijah

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sites',
-        'USER': 'bailey',
-        'PASSWORD': 'Bojangles60',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
